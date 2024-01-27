4c9210d33d13a12b0704f37cbc959541f074d06b
Richard Harris <rrricharrrd@gmail.com>
2024-01-27 16:35:08 +0900

    ENH: add soundwaves to Polyrhythm

--- polyrhythms/_polyrhythms.py
+++ polyrhythms/_polyrhythms.py
@@ -1,4 +1,3 @@
-# +
 import logging
 import math
 import time
@@ -6,7 +5,7 @@ import time
 import numpy as np
 import sounddevice as sd
 
-from .settings import A_FREQUENCY, BEEP_DURATION, E_FREQUENCY, SAMPLE_RATE, VOLUME_FACTOR
+from polyrhythms.settings import A_FREQUENCY, BEEP_DURATION, E_FREQUENCY, SAMPLE_RATE, VOLUME_FACTOR
 
 
 class Polyrhythm:
@@ -14,23 +13,36 @@ class Polyrhythm:
         self._beats = []
         self._freqs = []
         self._subdivision = 1
+        self._beat_interval = None
+        self._signals = []
+        self._duration = BEEP_DURATION
+        self._volume_factor = VOLUME_FACTOR
 
     def add_rhythm(self, beats: int, frequency: int = A_FREQUENCY):
         self._beats.append(beats)
         self._freqs.append(frequency)
         self._subdivision = math.lcm(*self._beats)
 
-    def play(self, beats_per_minute: int = 20):
-        beat_interval = 60 / beats_per_minute / self._subdivision
-        logging.debug(f"Interval {beat_interval}")
+    def set_tempo(self, bpm: int = 60):
+        self._beat_interval = 60 / bpm / self._subdivision
+        logging.debug(f"Interval {self._beat_interval}")
+        t = np.linspace(0, self._duration, int(self._duration * SAMPLE_RATE), endpoint=False)
+        self._signals = [self._volume_factor * np.sin(2 * np.pi * freq * t) for freq in self._freqs]
+
+    def play(self):
         while True:
             for i in range(self._subdivision):
-                beeps = ""
-                for b in self._beats:
+                signals = []
+                log_str = ""
+                for ix, b in enumerate(self._beats):
                     if i % b == 0:
-                        beeps += str(b)
-                print(beeps)
-            time.sleep(beat_interval - BEEP_DURATION)
+                        log_str += str(b)
+                        signals.append(self._signals[ix])
+                logging.debug(log_str)
+                if signals:
+                    signal = sum(signals)
+                    yield signal
+                time.sleep(self._beat_interval - BEEP_DURATION)
 
 
 # Thanks, ChatGPT...
@@ -61,7 +73,9 @@ def metronome(beats_per_minute, duration=BEEP_DURATION):
 
 if __name__ == "__main__":
     pr = Polyrhythm()
-    pr.add_rhythm(2)
-    pr.add_rhythm(3)
-
-    pr.play()
+    pr.add_rhythm(3, A_FREQUENCY)
+    pr.add_rhythm(4, E_FREQUENCY)
+    pr.set_tempo(40)
+    for beep in pr.play():
+        sd.play(beep, samplerate=SAMPLE_RATE)
+        sd.wait()
